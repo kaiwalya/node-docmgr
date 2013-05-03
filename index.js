96f05f234b19b354449a8bc5fa83820053382c77
Kaiwalya Kher <knkher@gmail.com>
2013-05-03 03:12:51 -0700

    Improve FindOptions, Add UpdateOptions.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var FindOptions = function (opt) {
 	var This = this;
 	if (opt) {
-		This.opt = opt;
+		if (opt.constructor = FindOptions) {
+			This.opt = opt.opt;
+		}
+		else {
+			This.opt = opt;
+		}
 	}
 	else {
 		This.opt = {};
 	}
 };
 
-FindOptions.prototype.first = function () {
-	this.opt.findType = 'first';
+FindOptions.prototype.single = function () {
+	this.opt.findType = 'single';
+	return this;
 };
 
-FindOptions.prototype.isFirst = function () {
-	return this.opt.findType === 'first';
+FindOptions.prototype.isSingle = function () {
+	return this.opt.findType === 'single';
 };
 
 FindOptions.prototype.done = function () {
@@ -21,3 +27,45 @@ FindOptions.prototype.done = function () {
 };
 
 exports.FindOptions = FindOptions;
+
+
+var UpdateOptions = function (opt) {
+	var This = this;
+	if (opt) {
+		if (opt.constructor = UpdateOptions) {
+			This.opt = opt.opt;
+		}
+		else {
+			This.opt = opt;
+		}
+	}
+	else {
+		This.opt = {
+		};
+	}
+};
+
+UpdateOptions.prototype.single = function () {
+	var This = this;
+	This.updateType = 'single';
+};
+
+UpdateOptions.prototype.isSingle = function () {
+	var This = this;
+	return This.updateType === 'single';
+};
+
+
+exports.UpdateOptions = UpdateOptions;
+
+var Ex = function (msg) {
+	Error.call(this, msg);
+};
+Ex.prototype = Object.create(Error.prototype);
+exports.Exception = Ex;
+
+var OOBEx = function () {
+	Ex.call(this, "OutOfBoundsException");
+};
+OOBEx.prototype = Object.create(Ex.prototype);
+exports.OutOfBoundsException = OOBEx;
